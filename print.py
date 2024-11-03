36339f408074e0f57901b2bf89b2e78ea4d9076c
ほっしーさんです <hoshitaku1228@gmail.com>
2024-11-03 21:05:15 +0900

    コメントアウト

--- print.py
+++ print.py
@@ -1,4 +1,4 @@
-name = "python"
-print("あああああ")
-print(name)
-print("あなたの名前は",name,"です")
+name = "python" #nameに好きな値を突っ込めるよ
+print("あああああ") #これも適当に変更したら変わるよ
+print(name) #nameの中身を表示
+print("あなたの名前は",name,"です") #文字列と変数を羅列して表示
