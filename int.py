b4faf4a71840352d0acb65449f4eec1e7763bec2
ほっしーさんです <hoshitaku1228@gmail.com>
2024-11-09 15:47:28 +0900

    追加

---
+++ int.py
@@ -0,0 +1,4 @@
+first_money = 5000
+getmoney = input("得たお金は?") #int(input("得たお金は"))でも可
+allmoney = first_money + int(getmoney) #int(変数)で変換
+print(allmoney)
---
+++ 計算例.py
@@ -0,0 +1,15 @@
+number1 = 10
+x = number1 + 10 
+print(x)
+y = number1 - 10
+print(y)
+z = number1 * 10
+print(z)
+a = number1 / 10
+print(a)
+b = number1 // 3
+print(b)
+c = number1 % 3
+print(c)
+d = number1 ** 3
+print(d)
